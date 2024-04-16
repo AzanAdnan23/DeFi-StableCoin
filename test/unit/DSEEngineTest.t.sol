272aa9ea5e201e4641c7f5de41a5e0952a6fcb2a
Azan Adnan <azanbutt2311@gmail.com>
2024-04-16 16:16:10 +0500

    Writing Tests for DSEEngine.sol

---
+++ test/unit/DSEEngineTest.t.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+    }
+}
