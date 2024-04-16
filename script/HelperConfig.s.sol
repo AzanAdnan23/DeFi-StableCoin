e2565076bd58d80efe115f710fc3c6c3778f2f3e
Azan Adnan <azanbutt2311@gmail.com>
2024-04-16 15:43:33 +0500

    Add MockV3Aggregator contract and import MockV3Aggregator in HelperConfig.s.sol

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
