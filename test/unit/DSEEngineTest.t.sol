17ca70a6476df5d04ddc0746cea12cecf882faa5
Azan Adnan <azanbutt2311@gmail.com>
2024-04-16 16:25:22 +0500

    Add HelperConfig and price test

--- test/unit/DSEEngineTest.t.sol
+++ test/unit/DSEEngineTest.t.sol
@@ -6,16 +6,26 @@ import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
 
 contract DSCEngineTest is Test {
     DSCEngine dsce;
     DecentralizedStableCoin dsc;
 
     DeployDSC deployer;
+    HelperConfig helperConfig;
+    address ethUsdPriceFeed;
+    address weth;
 
     function setUp() public {
         deployer = new DeployDSC();
 
-        (dsc, dsce) = deployer.run();
+        (dsc, dsce,helperConfig) = deployer.run();
     }
+
+    /**
+     * Price Test
+     */
+
+    function testGetUSDValue() 
 }
