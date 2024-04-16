e8680320e197f0b44cc32331060687e9e65d735c
Azan Adnan <azanbutt2311@gmail.com>
2024-04-17 00:24:10 +0500

    Update depositCollateralAndMintDsc function to deposit collateral and mint DSC in one transaction

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -102,7 +102,7 @@ contract DSCEngine is ReentrancyGuard {
      * @param _amountCollateral: The amount of collateral you're depositing
      */
     function depositCollateral(address _tokenCollateralAddress, uint256 _amountCollateral)
-        external
+        public
         MorethenZero(_amountCollateral)
         IsAllowedToken(_tokenCollateralAddress)
         nonReentrant
@@ -129,8 +129,21 @@ contract DSCEngine is ReentrancyGuard {
             revert DSCEngine__MintFailed();
         }
     }
+    /**
+     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
+     * @param amountCollateral: The amount of collateral you're depositing
+     * @param amountDscToMint: The amount of DSC you want to mint
+     * @notice This function will deposit your collateral and mint DSC in one transaction
+     */
 
-    function depositCollateralAndMintDSC() external {}
+    function depositCollateralAndMintDsc(
+        address tokenCollateralAddress,
+        uint256 amountCollateral,
+        uint256 amountDscToMint
+    ) external {
+        depositCollateral(tokenCollateralAddress, amountCollateral);
+        mintDsc(amountDscToMint);
+    }
 
     function redeemCollateral() external {}
 
