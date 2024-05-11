77b81c3fd2b9570eedec28e23d17a48b15b4992b
Azan Adnan <azanbutt2311@gmail.com>
2024-05-12 01:34:31 +0500

    Update foundry.toml and DSCEngine.sol, add getCollateralTokens() function and helper function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -376,6 +376,10 @@ contract DSCEngine is ReentrancyGuard {
     function getHealthFactor(address user) external view returns (uint256) {
         return _healthFactor(user);
     }
+
+    function getCollateralTokens() external view returns (address[] memory) {
+        return s_collateralTokens;
+    }
 }
 
 // Layout of Contract:
--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -6,17 +6,26 @@ import {Test, console} from "forge-std/Test.sol";
 
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "../../src/ERC20Mock.sol";
 
 contract Handler is Test {
     DSCEngine dsce;
     DecentralizedStableCoin dsc;
+    ERC20Mock weth;
+    ERC20Mock wbtc;
 
     constructor(DSCEngine _dsce, DecentralizedStableCoin _dsc) {
         dsce = _dsce;
         dsc = _dsc;
     }
 
-    function depositCollateral(address collateral, uint256 amountCollateral) public {
+    function depositCollateral( uint256 collateralSeed, uint256 amountCollateral) public {
         dsce.depositCollateral(collateral, amountCollateral);
+    }
+
+    //Hekper Function
+    function _getCollateralFromSeed( uint256 collateralSeed) private vieew returns (ERC20Mock)
+    {
+        if (collateralSeed %2 =)
     }
 }
