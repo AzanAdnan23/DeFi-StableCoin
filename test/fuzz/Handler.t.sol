e1d174d1b2713cdf1ad004ea24d7029b377d9a3f
Azan Adnan <azanbutt2311@gmail.com>
2024-05-12 02:40:55 +0500

    Update foundry.toml and Handler contract

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -6,26 +6,40 @@ import {Test, console} from "forge-std/Test.sol";
 
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
-import {ERC20Mock} from "../../src/ERC20Mock.sol";
+import {ERC20Mock} from "../../test/mocks/ERC20Mock.sol";
 
 contract Handler is Test {
     DSCEngine dsce;
     DecentralizedStableCoin dsc;
     ERC20Mock weth;
     ERC20Mock wbtc;
+    uint256 MAX_DEPOSI_SIZE = type(uint96).max;
 
     constructor(DSCEngine _dsce, DecentralizedStableCoin _dsc) {
         dsce = _dsce;
         dsc = _dsc;
+
+        address[] memory collateralTokens = dsce.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
     }
 
-    function depositCollateral( uint256 collateralSeed, uint256 amountCollateral) public {
-        dsce.depositCollateral(collateral, amountCollateral);
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSI_SIZE);
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(dsce), amountCollateral);
+        dsce.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
     }
 
     //Hekper Function
-    function _getCollateralFromSeed( uint256 collateralSeed) private vieew returns (ERC20Mock)
-    {
-        if (collateralSeed %2 =)
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+        if (collateralSeed % 2 == 0) {
+            return weth;
+        } else {
+            return wbtc;
+        }
     }
 }
