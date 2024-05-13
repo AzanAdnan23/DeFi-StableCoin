e5b253c7d46c4522dc67fb65d419aa2ab752e7da
Azan Adnan <azanbutt2311@gmail.com>
2024-05-14 01:36:09 +0500

    Add getCollateralTokenPriceFeed function and MockV3Aggregator import

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -8,12 +8,16 @@ import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 import {console} from "forge-std/console.sol";
+import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
 
 contract Handler is Test {
     DSCEngine public dsce;
     DecentralizedStableCoin public dsc;
     ERC20Mock public weth;
     ERC20Mock public wbtc;
+
+    MockV3Aggregator public ethUsdPriceFeed;
+    MockV3Aggregator public btcUsdPriceFeed;
 
     uint256 public count = 0;
 
@@ -27,6 +31,9 @@ contract Handler is Test {
         address[] memory collateralTokens = dsce.getCollateralTokens();
         weth = ERC20Mock(collateralTokens[0]);
         wbtc = ERC20Mock(collateralTokens[1]);
+
+        ethUsdPriceFeed = MockV3Aggregator(dsce.getCollateralTokenPriceFeed(address(weth)));
+        btcUsdPriceFeed = MockV3Aggregator(dsce.getCollateralTokenPriceFeed(address(wbtc)));
     }
 
     function mintAndDepositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
@@ -53,6 +60,15 @@ contract Handler is Test {
         vm.prank(msg.sender);
         dsce.redeemCollateral(address(collateral), amountCollateral);
     }
+
+    // This breaks our invariants test suite
+    // function updateCollateralPrice(uint96 newPrice, uint256 collateralSeed) public {
+    //     int256 intNewPrice = int256(uint256(newPrice));
+    //     ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+    //     MockV3Aggregator priceFeed = MockV3Aggregator(dsce.getCollateralTokenPriceFeed(address(collateral)));
+
+    //     priceFeed.updateAnswer(intNewPrice);
+    // }
 
     //Helper Function
     function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
